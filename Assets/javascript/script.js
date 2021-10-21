d09b94a4623a9622ed78b36c902d473c1a1bf390
Kay Oss <mne36325i@gmail.com>
2021-10-21 12:04:46 -0500

    figured out structure for changing colors of text boxes through javascript

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,4 +1,153 @@
-var description = document.getElementById("description")
+var description = document.getElementById("description");
+var dayArea = document.getElementById("currentDay");
+const today = moment();
+var format = 'h:mm';
 
-console.log(description.textContent);
+dayArea.textContent += today.format('MM-DD-YYYY');
 
+
+
+//Finding time for each time block and determining if it should be red, green, or grey.
+var text1 = document.getElementById("description1");
+var timeblock4 = moment('11:15:00',format),
+beforeTime = moment('12:00:00',format),
+afterTime = moment('12:59:59',format);
+
+if (timeblock4.isBefore(beforeTime)){
+    text1.style.backgroundColor = "darkgray";
+
+} else {
+    text1.style.backgroundColor = "green";
+}
+if (timeblock4.isBetween(beforeTime, afterTime)){
+    text1.style.backgroundColor = "red";
+}
+
+var text2 = document.getElementById("description2");
+var timeblock4 = moment('13:15:00',format),
+beforeTime = moment('12:00:00',format),
+afterTime = moment('12:59:59',format);
+
+if (timeblock4.isBefore(beforeTime)){
+    text2.style.backgroundColor = "darkgray";
+
+} else {
+    text2.style.backgroundColor = "green";
+}
+if (timeblock4.isBetween(beforeTime, afterTime)){
+    text2.style.backgroundColor = "red";
+}
+
+
+var timeblock4 = moment('13:15:00',format),
+beforeTime = moment('12:00:00',format),
+afterTime = moment('12:59:59',format);
+
+if (timeblock4.isBefore(beforeTime)){
+    description.style.backgroundColor = "darkgray";
+
+} else {
+    description.style.backgroundColor = "green";
+}
+if (timeblock4.isBetween(beforeTime, afterTime)){
+    description.style.backgroundColor = "red";
+}
+
+
+var timeblock4 = moment('13:15:00',format),
+beforeTime = moment('12:00:00',format),
+afterTime = moment('12:59:59',format);
+
+if (timeblock4.isBefore(beforeTime)){
+    description.style.backgroundColor = "darkgray";
+
+} else {
+    description.style.backgroundColor = "green";
+}
+if (timeblock4.isBetween(beforeTime, afterTime)){
+    description.style.backgroundColor = "red";
+}
+
+
+var timeblock4 = moment('13:15:00',format),
+beforeTime = moment('12:00:00',format),
+afterTime = moment('12:59:59',format);
+
+if (timeblock4.isBefore(beforeTime)){
+    description.style.backgroundColor = "darkgray";
+
+} else {
+    description.style.backgroundColor = "green";
+}
+if (timeblock4.isBetween(beforeTime, afterTime)){
+    description.style.backgroundColor = "red";
+}
+
+
+var timeblock4 = moment('13:15:00',format),
+beforeTime = moment('12:00:00',format),
+afterTime = moment('12:59:59',format);
+
+if (timeblock4.isBefore(beforeTime)){
+    description.style.backgroundColor = "darkgray";
+
+} else {
+    description.style.backgroundColor = "green";
+}
+if (timeblock4.isBetween(beforeTime, afterTime)){
+    description.style.backgroundColor = "red";
+}
+
+
+var timeblock4 = moment('13:15:00',format),
+beforeTime = moment('12:00:00',format),
+afterTime = moment('12:59:59',format);
+
+if (timeblock4.isBefore(beforeTime)){
+    description.style.backgroundColor = "darkgray";
+
+} else {
+    description.style.backgroundColor = "green";
+}
+if (timeblock4.isBetween(beforeTime, afterTime)){
+    description.style.backgroundColor = "red";
+}
+
+
+var timeblock4 = moment('13:15:00',format),
+beforeTime = moment('12:00:00',format),
+afterTime = moment('12:59:59',format);
+
+if (timeblock4.isBefore(beforeTime)){
+    description.style.backgroundColor = "darkgray";
+
+} else {
+    description.style.backgroundColor = "green";
+}
+if (timeblock4.isBetween(beforeTime, afterTime)){
+    description.style.backgroundColor = "red";
+}
+
+
+var timeblock4 = moment('13:15:00',format),
+beforeTime = moment('12:00:00',format),
+afterTime = moment('12:59:59',format);
+
+if (timeblock4.isBefore(beforeTime)){
+    description.style.backgroundColor = "darkgray";
+
+} else {
+    description.style.backgroundColor = "green";
+}
+if (timeblock4.isBetween(beforeTime, afterTime)){
+    description.style.backgroundColor = "red";
+}
+
+
+
+
+
+
+// if(
+//     moment().format('h:mm').isSameOrAfter()
+// );
